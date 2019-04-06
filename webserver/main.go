b07b2bf8743990e7fed03c3aa619e78cb3b00118
1348985957 <1348985957@qq.com>
2019-04-06 14:17:35 +0800

    change the web text

--- webserver/main.go
+++ webserver/main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func firstPage(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "<h1>Hello, Golang!</h1>")
+	io.WriteString(w, "<h1>Hello, I am ycx!</h1>")
 }
 
 func main() {
